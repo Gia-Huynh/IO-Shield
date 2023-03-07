0be6bc718822eafd715a0512c88842ee704b3cd2
Gia-Huynh <huynhthietgia123@gmail.com>
2023-03-07 14:09:29 +0700

    Initial commit

---
+++ gay_code.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+gay_image = cv2.imread ("B450M Pro4-F(L5).png")
+cv2.imshow ('test', gay_image)
+gray = cv2.cvtColor(gay_image, cv2.COLOR_BGR2GRAY)
+print (gray.dtype)
+gray [gray < 26] = 0
+gray [gray > 25] -= 25
+gray = gray * 1.1
+gray = np.clip (gray, 0, 255).astype (np.uint8)
+gray = cv2.medianBlur(gray, 9)
+rows = gray.shape[0]
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows/8,
+                               param1=80, param2=30,
+                               minRadius=int(rows/10), maxRadius=int(rows/4))
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        center = (i[0], i[1])
+        # circle center
+        cv2.circle(gray, center, 1, (0, 100, 100), 3)
+        # circle outline
+        radius = i[2]
+        cv2.circle(gray, center, radius, (255, 0, 255), 3)
+
+
+cv2.imshow("detected circles", gray)
+cv2.waitKey(0)
