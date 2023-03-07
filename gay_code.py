f6e7e4e9609ced3bf1e50d2393b9618c60f845f8
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2023-03-07 16:29:17 +0700

    yay

--- gay_code.py
+++ gay_code.py
@@ -1,7 +1,27 @@
-import cv2
+import cv2	
 import numpy as np
-gay_image = cv2.imread ("B450M Pro4-F(L5).png")
-#gay_image = cv2.imread ("GA-H110-D3A.webp")
+from collections import deque
+#filename = "B450M Pro4-F(L5).png"
+filename = "GA-H110-D3A.webp"
+
+def niggaBFS (image):
+	image = cv2.medianBlur(image, 3)
+	gay_color = image [0, 0]
+	print (image.shape)
+	N = (image.shape)[0]
+	M = image.shape[1]
+	visited = np.full_like(image[:,:,0], 0)
+	
+	visited = np.where(np.all(image == gay_color,axis=2), 1, 0).astype (np.uint8)
+	print (np.where(np.any(image == gay_color)))
+	print (visited.shape)
+	cv2.imshow("BFS", visited*255)
+	return visited
+	
+gay_image = cv2.imread (filename)
+gay_hsv = cv2.cvtColor(gay_image, cv2.COLOR_BGR2HSV)
+niggaBFS (gay_image)
+
 cv2.imshow ('test', gay_image)
 gray = cv2.cvtColor(gay_image, cv2.COLOR_BGR2GRAY)
 print (gray.dtype)
@@ -14,7 +34,7 @@ rows = gray.shape[0]
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows/8,
                                param1=80, param2=20,
                                minRadius=int(rows/10), maxRadius=int(rows/8))
-if circles is not None:
+if circles is not None:	
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         center = (i[0], i[1])
