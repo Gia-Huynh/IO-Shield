1834bb1e1703bd268ee349acd715fb038463b3ff
Gia-Huynh <huynhthietgia123@gmail.com>
2023-03-16 15:25:56 +0700

    file upload done (?)

--- Website/logic.js
+++ Website/logic.js
@@ -5,6 +5,25 @@ ad_list_arr.forEach(i => {
 	  this.style.visibility = 'hidden';
 	};
 });
-/*.onclick = function() {
-  this.style.display = 'none';
-};*/
+
+
+
+dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
+  evt.preventDefault();
+};
+
+dropContainer.ondrop = function(evt) {
+  // pretty simple -- but not for IE :(
+ 
+  evt.preventDefault();
+  InputBox.files = evt.dataTransfer.files;
+
+  // If you want to use some of the dropped files
+  const dT = new DataTransfer();
+  dT.items.add(evt.dataTransfer.files[0]);
+  //dT.items.add(evt.dataTransfer.files[3]);
+  InputBox.files = dT.files;
+	
+
+  evt.preventDefault();
+};
