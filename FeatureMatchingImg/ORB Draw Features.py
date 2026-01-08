96a6a22097f53e0397b5138086198786eafc5c83
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2026-01-07 23:50:31 -0500

    Adding Text Label, Stable Backup
    Fucking up the blur ratio later

--- FeatureMatchingImg/ORB Draw Features.py
+++ FeatureMatchingImg/ORB Draw Features.py
@@ -43,16 +43,53 @@ def cleaningConnectedCompoents(inp_img):
         if ((areas_sorted[i] > top_cap) or #If region too big
             (areas_sorted[i] < bot_cap) or #If region too small
             (areas_sorted[i] < circle_cap and ((temp_height/temp_width > 1.4) or (temp_width/temp_height>1.4) or (temp_height*temp_width/components_sorted[i]["area"]>1.25))) or #If region is kinda small but not a circle shape, or has huge hole in middle or weird shape
-            (temp_height/temp_width > 2) or #If region too "tall"
+            (temp_height/temp_width > 1.25) or #If region too "tall"
             (temp_width/temp_height>5) or #If region too "wide"
             (temp_height*temp_width/components_sorted[i]["area"]>1.5)): #If region shape is too weird (bounding box is twice as large as area)
             delete_mask = delete_mask + components_sorted[i]["mask"]
         else:
-            keep_mask = keep_mask + components_sorted[i]["mask"]
-            
+            keep_mask = keep_mask + components_sorted[i]["mask"]            
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (6, 6))
     keep_mask_closed = cv.morphologyEx(keep_mask, cv.MORPH_CLOSE, kernel, iterations=1)
     return keep_mask_closed
+port_id_dict = {"ethernet":1, 1:"ethernet",
+                "usb":2, 2:"usb",
+                "audio":3, 3:"audio",
+                "ps2-wifi":4, 4:"ps2-wifi"
+                }
+def port_detect_heuristic (SingleConnectedComponent):
+    x, y, w, h = SingleConnectedComponent["bbox"]
+    area = SingleConnectedComponent["area"]
+    mask = SingleConnectedComponent["mask"]
+    centroid = SingleConnectedComponent["centroid"]
+    print (w, h)
+    if (w > 75) and     (85 > h) and    (h > 55):
+        return port_id_dict ["ethernet"]
+    if (90 > w) and     (w  > 50) and   (45 > h) and (h > 15):
+        return port_id_dict ["usb"]
+    if (w/h > 0.75) and (h/w > 0.75):  #If circular
+        if (40 > w) and (40 > h): #And small size
+            return port_id_dict ["audio"]
+        else:
+            return port_id_dict ["ps2-wifi"]
+def textOverlayOnImage (img, text_string, x, y):
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (x, y)
+    fontScale              = 1
+    fontColor              = (125) #(125,125,125)
+    thickness              = 1
+    lineType               = 2
+    print (text_string, bottomLeftCornerOfText)
+    img = cv2.putText(  img,
+                        text_string, 
+                        bottomLeftCornerOfText, 
+                        font, 
+                        fontScale,
+                        fontColor,
+                        thickness,
+                        lineType)
+    return img
+
 def show_Canny (img_path):
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(12,12))
@@ -63,52 +100,33 @@ def show_Canny (img_path):
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (6, 6))
     #kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
     closed = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel, iterations=2)
-    #closed = cv2.copyMakeBorder(closed,1,1,1,1,cv2.BORDER_CONSTANT,value=0)
-    #closed_thinned = cv.ximgproc.thinning(
-    #    closed,
-    #    thinningType=cv.ximgproc.THINNING_ZHANGSUEN
-    #)
-    #closed = closed_thinned
+    #closed_thinned = cv.ximgproc.thinning(closed,thinningType=cv.ximgproc.THINNING_ZHANGSUEN)
     closed_invert = np.max(closed) - closed
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))
     closed_invert = cv.morphologyEx(closed_invert, cv.MORPH_OPEN, kernel, iterations=1)
     closed_invert = cv2.copyMakeBorder(closed_invert,1,1,1,1,cv2.BORDER_CONSTANT,value=255)
 
-    #
-    """components_sorted, areas_sorted = getConnectedWithStat (closed_invert)    
-    top_cap = 9000
-    bot_cap = 200
-    circle_cap = 1000
-    delete_mask = np.zeros(components_sorted[0]["mask"].shape)
-    keep_mask = np.zeros(components_sorted[0]["mask"].shape)
-    for i in range (0, len(components_sorted)):
-        temp_width = components_sorted[i]["bbox"][2]
-        temp_height = components_sorted[i]["bbox"][3]
-        if ((areas_sorted[i] > top_cap) or #If region too big
-            (areas_sorted[i] < bot_cap) or #If region too small
-            (areas_sorted[i] < circle_cap and ((temp_height/temp_width > 1.4) or (temp_width/temp_height>1.4) or (temp_height*temp_width/components_sorted[i]["area"]>1.25))) or #If region is kinda small but not a circle shape, or has huge hole in middle or weird shape
-            (temp_height/temp_width > 2) or #If region too "tall"
-            (temp_width/temp_height>5) or #If region too "wide"
-            (temp_height*temp_width/components_sorted[i]["area"]>1.5)): #If region shape is too weird (bounding box is twice as large as area)
-            delete_mask = delete_mask + components_sorted[i]["mask"]
-        else:
-            keep_mask = keep_mask + components_sorted[i]["mask"]
-            
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (6, 6))
-    keep_mask_closed = cv.morphologyEx(keep_mask, cv.MORPH_CLOSE, kernel, iterations=1)"""
-    keep_mask_closed = np.copy(closed_invert)
 
     old_components_sorted, old_areas_sorted = None, None
     gay_list = []
+    keep_mask_closed = np.copy(closed_invert)
     while True:
         keep_mask_closed = cleaningConnectedCompoents (keep_mask_closed.astype (np.uint8))
         components_sorted, areas_sorted = getConnectedWithStat (keep_mask_closed.astype (np.uint8))
-        if (old_areas_sorted is not None) and (areas_sorted == old_areas_sorted):
-            break
-        else:
+        if (old_areas_sorted is None) or (areas_sorted != old_areas_sorted):
             old_areas_sorted = areas_sorted
             gay_list.append (keep_mask_closed)
+        else:
+            break
     del old_components_sorted, old_areas_sorted
+    text_overlayed_img = np.copy(keep_mask_closed)
+    for component in components_sorted:
+        port_id = port_detect_heuristic (component)
+        if (port_id is None):
+            continue
+        text_overlayed_img = textOverlayOnImage (text_overlayed_img, port_id_dict[port_id], component["bbox"][0], component["bbox"][1])
+    gay_list.append (text_overlayed_img)
+        
     img = np.vstack((img, edges, closed, closed_invert[1:-1, 1:-1])) #, closed_thinned
     cv2.imshow(img_path, img)
     mask_process_img = np.vstack(gay_list) #delete_mask, keep_mask, 
